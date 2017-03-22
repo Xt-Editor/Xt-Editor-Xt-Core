88e435b520924adb23791ca899d21c44a093f6b1
Dom Rodriguez <shymega@shymega.org.uk>
2017-03-22 17:04:58 +0000

    Add license header to source code (GPLv3)

--- src/bin/xtc.rs
+++ src/bin/xtc.rs
@@ -1,3 +1,22 @@
+//! This module serves as the entry point into Xtensis's main binary.
+
+// This is the Xtensis text editor; it edits text.
+// Copyright (C) 2016-2017  {The Xtensis Developers}
+
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+
 fn main() {
     println!("Hello, XTENSIS!");
 }
