a792e61c1c499cb8192af6d26181cccde96f45f5
Dom Rodriguez <shymega@shymega.org.uk>
2018-03-21 21:19:58 +0000

    Update module documentation

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -19,7 +19,19 @@
 // along with this program.  If not, see
 // <http://www.gnu.org/licenses/>.
 
-extern crate gapbuffer;
-
-/// Buffer module
+/// `Buffer` struct module for Xt.
+///
+/// Each struct instance holds a Buffer's state, and links with the
+/// associated workspace of the buffer.
+/// If no workspace is defined, it uses the wildcard workspace.
 pub mod buffer;
+
+/// Workspace module for Xt.
+/// Holds a collection of `Buffer` structs together with
+/// `Vec<Buffers>`.
+pub mod workspace;
+
+/// Major and Minor modes for Xt.
+/// Defines the basic structure for each modes, with (planned)
+/// extensible.
+pub mod modes;
