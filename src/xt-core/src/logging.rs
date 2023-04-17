26850575b7de8e87a8c1be31df39ffb419ad4c6b
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-17 23:32:27 +0000

    Simplify slog init fn
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/xt-core/src/logging.rs
+++ src/xt-core/src/logging.rs
@@ -4,11 +4,11 @@ use slog::{o, Drain, Logger};
 use slog_term::{FullFormat, TermDecorator};
 use std::sync::Mutex;
 
-/// Logger initialistion function.
-pub fn init_logger() -> Option<Logger> {
+/// Logger initialisation function.
+pub fn init_logger() -> Logger {
     let decorator = TermDecorator::new().build();
     let drain = FullFormat::new(decorator).build().fuse();
     let drain = Mutex::from(drain).fuse();
 
-    Some(Logger::root(drain, o!()))
+    Logger::root(drain, o!())
 }
