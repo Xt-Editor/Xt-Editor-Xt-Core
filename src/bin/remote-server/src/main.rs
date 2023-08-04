8c69da7533ab9d92ec5212d7199d697037a6d15b
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-04 17:50:38 +0100

    Reorganise workspace structure

---
+++ src/bin/remote-server/src/main.rs
@@ -0,0 +1,22 @@
+//! Remote daemon for Xt.
+//! This handles remote communications over remote sessions, such as SSH, or via pipes.
+#![deny(
+    warnings,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::pedantic,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
+
+fn main() {
+    unimplemented!();
+}
